8a3e167a87ae4c661e9bea2843654928fc35d1c0
Bruno Monteiro <malettabruno@gmail.com>
2021-01-31 20:12:47 -0300

    Add stresstest for sms

---
+++ stress_test/sms/a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "../../src/sms.cpp"
+
+int main() {
+	int q;
+	std::cin >> q;
+	sms<int> s;
+	while (q--) {
+		char c;
+		std::cin >> c;
+		if (c == '+') {
+			int x;
+			std::cin >> x;
+			s.insert(x);
+		} else if (c == '+') {
+			int x;
+			std::cin >> x;
+			s.erase(x);
+		} else if (c == '?') {
+			int x;
+			std::cin >> x;
+			if (s.count(x)) std::cout << 1 << std::endl;
+			else std::cout << 0 << std::endl;
+		}
+	}
+	return 0;
+}
---
+++ stress_test/sms/gen.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+const int LIM = 1e6;
+
+int main(int argc, char** argv) {
+	srand(atoi(argv[1]));
+	int q = rand()%LIM+1;
+	std::cout << q << std::endl;
+	while (q--) {
+		int t = rand()%4;
+		if (t == 0) std::cout << "+ ";
+		if (t == 1) std::cout << "- ";
+		if (t == 2) std::cout << "? ";
+		std::cout << rand()%LIM << std::endl;
+	}
+	exit(0);
+}
+
